7cd14851597ae7e92bbd1c8e905ce9a7cbddf0ce
aethernet <aethernet65535@gmail.com>
2025-02-11 15:55:45 +0800

    update

--- main.asm
+++ main.asm
@@ -1,18 +1,39 @@
+section .bss
+    buffer resb 100
+    userInput resb 100
+
 section .data
-    msg db "你好世界！", 0xA 
-    len equ $ - msg
+    prompt db "请输入文字：", 0 
+    promptLen equ $ - prompt
 
 section .text
     global _start
 
 _start:
+    ;提示输出信息   
     mov rax, 1
     mov rdi, 1
-    mov rsi, msg
-    mov rdx, len
+    mov rsi, prompt
+    mov rdx, promptLen
     syscall
 
+    ;读取用户输入
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, userInput
+    mov rdx, 100
+    syscall
+
+    mov rbx, rax
+
+    ;输出用户输入
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, userInput
+    mov rdx, rbx
+    syscall
+
+    ;退出
     mov rax, 60
     xor rdi, rdi
     syscall
-
