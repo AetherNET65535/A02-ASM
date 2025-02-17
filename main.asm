06fcc42036a37b747baa68dc5cba4bfef5f4699f
aethernet65535 <aethernet65535@gmail.com>
2025-02-18 05:04:43 +0800

    Auto update

--- main.asm
+++ main.asm
@@ -1,87 +1,60 @@
-section .bss
-    userInput resb 100
-
 section .data
-    prompt db "请输入您的名字：", 0xA
-    promptLen equ $ - prompt
-    
-    shortMsg db "小杂鱼♡～名字这么短，估计那玩意也差不多吧♡～", 0xA
-    shortLen equ $ - shortMsg
-    
-    longMsg db "名字这么长吗？还是说那里太短了所以故意把名字弄长呢？果然是个小杂鱼呢♡～", 0xA
-    longLen equ $ - longMsg
-    
-    normalMsg db "小杂鱼♡～你也就只有名字的长度是正常了吧，杂鱼♡～杂鱼♡～", 0xA
-    normalLen equ $ - normalMsg
+    prompt db "请输入数字：", 0xA
+    prompt_len equ $ - prompt
+    finish_msg db "已结束！", 0xA
+    finish_len equ $ - finish_msg
+
+section .bss
+    user_input resb 100   ; 分配100字节的内存，用于存储用户输入
 
 section .text
     global _start
 
 _start:
-    ;提示输出信息   
+    ; 提示用户输入
     mov rax, 1
     mov rdi, 1
     mov rsi, prompt
-    mov rdx, promptLen
+    mov rdx, prompt_len
     syscall
 
-    ;读取用户输入
+    ; 读取用户输入
     mov rax, 0
     mov rdi, 0
-    mov rsi, userInput
+    mov rsi, user_input
     mov rdx, 100
     syscall
 
-    ;移除回车换行
-    mov rbx, rax ;获取用户字节数
-    cmp rbx, 1 ;除了回车，什么都会+回车都会大于1
-    jle return_0 ;所以能触发的只能是空了
-    
-    mov rax, userInput ;rax去第一个字母s
-    add rax, rbx ;rax去回车
-    mov byte [rax-1], 0 ;去rax的回车那里替换为结束
+    ; 计算（转换字符串为数字，加1）
+    mov rsi, user_input         ; rsi 指向 user_input
+    xor rbx, rbx                ; 清空 rbx（将其用作存储数字）
 
-    ;看看长短
-    cmp rbx, 10
-    jle short_name
+convert_loop:
+    movzx rax, byte[rsi]               ; 将当前字符加载到 rax
+    test rax, rax                 ; 检查是否是字符串结束符（0x0A）
+    jz done_convert             ; 如果是 0x0A（换行符），结束转换
 
-    cmp rbx, 50
-    jg long_name
-    
-    jmp normal_name
+    sub rax, '0'                 ; 将字符转换为数字（'0' -> 0, '1' -> 1, ..., '9' -> 9）
+    imul rbx, rbx, 10           ; 将现有数字乘以 10
+    add rbx, rax                 ; 加上当前的数字
 
-;短短函数
-short_name:
+    inc rsi                     ; 移动到下一个字符
+    jmp convert_loop            ; 继续转换
+
+done_convert:
+    add rbx, 1                 ; 加1（例如：65535 -> 65536）
+
+    ; 输出计算结果
+    mov rsi, rbx               ; 将结果存入 rsi（输出结果）
+    ; 这里可以将 rsi 转回字符串输出（需要进一步实现）
+
+    ; 结束
     mov rax, 1
     mov rdi, 1
-    mov rsi, shortMsg
-    mov rdx, shortLen
+    mov rsi, finish_msg
+    mov rdx, finish_len
     syscall
-    
-    jmp return_0
 
-;长长函数
-long_name:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, longMsg
-    mov rdx, longLen
-    syscall
-    
-    jmp return_0
-
-;正常函数
-normal_name:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, normalMsg
-    mov rdx, normalLen
-    syscall
-    
-    jmp return_0
-
-;退出
-return_0:
     mov rax, 60
     xor rdi, rdi
     syscall
