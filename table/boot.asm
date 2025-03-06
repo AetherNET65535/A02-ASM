cfb992b0652234c69f5f2f7c4ab89f7694fefe5e
AetherNET65535 <aethernet65535@gmail.com>
2025-03-06 13:50:00 +0800

    Auto push: 2025-03-06 13:50:00

--- table/boot.asm
+++ table/boot.asm
@@ -1,32 +1,13 @@
-[org 0x7C00]
-mov ah, 0x0E
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, ','
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 'w'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'r'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'd'
-int 0x10
-mov al, '!'
-int 0x10
-jmp $
+[bits 16]
+org 0x7C00
 
-times 510- ($-$$) db 0
+start:
+    mov ah, 0x00
+    int 0x16
+    mov ah, 0x0E
+    int 0x10
+
+    jmp start
+
+times 510 - ($-$$) db 0
 dw 0xAA55
