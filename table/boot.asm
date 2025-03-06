88602904193cbcdb0f73bd01ce4dfd144a79193b
AetherNET65535 <aethernet65535@gmail.com>
2025-03-06 14:30:00 +0800

    Auto push: 2025-03-06 14:30:00

--- table/boot.asm
+++ table/boot.asm
@@ -1,21 +1,13 @@
 [bits 16]
-org 0x7C00
+org 0x7C00  ; 传统 BIOS 启动扇区地址
 
-print:
-    mov ah, 0x0E
-    mov al, 'H'  ; 先输出一个标记
-    int 0x10
-    mov al, 'E'
-    int 0x10
-    mov al, 'X'
-    int 0x10
+start:
+    mov ah, 0x10   ; 读取按键
+    int 0x16       ; BIOS 键盘输入中断
+    mov ah, 0x0E   ; 让 BIOS 在屏幕上显示字符
+    int 0x10       ; 显示 AL 里的字符
 
-    mov al, '0' + ((al >> 4) & 0x0F)  ; 取高 4 位
-    int 0x10
-    mov al, '0' + (al & 0x0F)         ; 取低 4 位
-    int 0x10
+    jmp start      ; 继续读取输入
 
-    jmp start
-
-times 510 - ($-$$) db 0
-dw 0xAA55
+times 510-($-$$) db 0
+dw 0xAA55  ; BIOS 启动标志
